92e0ae1846d02bee84c30a4a25ecedcbb2114608
Jurgens Smit <contact@jurgenssmit.com>
2013-07-02 16:16:42 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheckingCreditCards::Application.config.secret_token = '0bd98d5f0a734e87ea4311f4fa3f9bddd37168710cb0bfd612d73d2d6337869393b3c61ab00ad60893726c00d80075b81d1f046cfd11e7013f4cf41c594f0b8c'
